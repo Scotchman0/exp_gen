04d8c791cc7abeb8007b5cd77955e7b16d9bab27
Will Russell <will.russell.01@gmail.com>
2021-06-15 14:13:04 -0400

    minor patch

--- install.sh
+++ install.sh
@@ -4,3 +4,16 @@
 #check for version mismatch:
 check_version=$(grep "v:" ~/.exp_gen/exp_gen.conf)
 echo $check_version
+
+
+uninstall () {
+# confirms user wants to uninstall/delete all exp + level data then purges files:
+# removes ~/.exp_gen/ recursively, clearing all install files
+# removes copy of 'exp' from ~/bin
+echo "uninstall completed"
+rm ~/.exp_gen/my_level
+rm ~/.exp_gen/my_exp
+rm ~/.exp_gen/exp_gen.conf
+rmdir ~/.exp_gen
+exit 0
+}
